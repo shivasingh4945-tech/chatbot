860b32de732eea04848b75d93767dc7f3e63a4e7
shiva <shivasingh4945@gmail.com>
2026-04-14 15:24:07 +0530

    update 2

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -6,4 +6,29 @@ from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
 
+load_dotenv()
+
+llm = ChatOpenAI()
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage],add_messages]
+
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+#checkpointer
+
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+
+graph.add_node("chat_node",chat_node)
+graph.add_edge(START,"chat_node")
+graph.add_edge("chat_node",END)
+
+chatbot = graph.compile(checkpointer = checkpointer)
+
 
---
+++ langgraph_frontend.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+
+with st.chat_message('user'):
+    st.text('Hi')
+
+with st.chat_message('assistant'):
+    st.text('How can I help you?') 
+
+user_input = st.chat_input('Type here')
+if user_input:
+    with st.chat_message('user'):
+        st.text(user_input)
