fbace3e374b2be0da212fed41f1346968784edd0
shiva <shivasingh4945@gmail.com>
2026-04-14 20:08:21 +0530

    basic_chatbot

--- langgraph_frontend.py
+++ langgraph_frontend.py
@@ -1,12 +1,46 @@
 import streamlit as st 
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
 
-with st.chat_message('user'):
-    st.text('Hi')
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
-with st.chat_message('assistant'):
-    st.text('How can I help you?') 
+# Initialize session state
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
 
+# Display chat history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+# User input
 user_input = st.chat_input('Type here')
+
 if user_input:
+
+    # Store user message
+    st.session_state['message_history'].append({
+        'role': 'user',
+        'content': user_input
+    })
+
     with st.chat_message('user'):
         st.text(user_input)
+
+    #  LangGraph call
+    response = chatbot.invoke(
+        {'messages': [HumanMessage(content=user_input)]},
+        config=CONFIG
+    )
+
+    # Extract AI response
+    ai_message = response['messages'][-1].content
+
+    # Store assistant message
+    st.session_state['message_history'].append({
+        'role': 'assistant',
+        'content': ai_message
+    })
+
+    with st.chat_message('assistant'):
+        st.text(ai_message)
